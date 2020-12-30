54f09c1a86ea51cda5d5d79a1dfbf1df42e6c380
sreguera <sreguera@gmail.com>
2020-12-30 16:53:28 +0100

    Create transputer class and tests

---
+++ src/Transputer.test.ts
@@ -0,0 +1,28 @@
+import { Transputer } from './Transputer'
+
+test('ldc loads a constant', () => {
+    const t = new Transputer();
+    t.writeByteMem(0, 0x43);
+    t.step();
+    expect(t.top()).toBe(0x3);
+});
+
+test('ldc loads a long constant', () => {
+    const t = new Transputer();
+    t.writeByteMem(0, 0x23);
+    t.writeByteMem(1, 0x24);
+    t.writeByteMem(2, 0x45);
+    t.step();
+    t.step();
+    t.step();
+    expect(t.top()).toBe(0x345);
+});
+
+test('ldc loads a negative constant', () => {
+    const t = new Transputer();
+    t.writeByteMem(0, 0x60);
+    t.writeByteMem(1, 0x4E);
+    t.step();
+    t.step();
+    expect(t.top()).toBe(-2);
+});
---
+++ src/Transputer.ts
@@ -0,0 +1,137 @@
+export {}
+
+enum Regs { Areg, Breg, Creg, Wptr, Iptr, Oreg, Eoreg };
+
+const MostNeg = 0x80000000;
+const MostPos = 0x7FFFFFFF;
+
+export class Transputer {
+
+    registers: Int32Array = new Int32Array(Regs.Eoreg);
+
+    memory: Uint8Array = new Uint8Array(4096);
+
+    constructor() {
+        this.writeIptr(0);
+    }
+
+    step() {
+        const inst = this.readByteMem(this.readIptr());
+        this.writeOreg(this.readOreg() | (inst & 0xF));
+        switch ((inst & 0xF0) >>> 4) {
+            case 0x0: {
+                break;
+            }
+            case 0x1: {
+                break;
+            }
+            case 0x2: {
+                this.execPfix();
+                break;
+            }
+            case 0x3: {
+                break;
+            }
+            case 0x4: {
+                this.execLdc();
+                break;
+            }
+            case 0x5: {
+                break;
+            }
+            case 0x6: {
+                this.execNfix();
+                break;
+            }
+            case 0x7: {
+                break;
+            }
+            case 0x8: {
+                break;
+            }
+            case 0x9: {
+                break;
+            }
+            case 0xA: {
+                break;
+            }
+            case 0xB: {
+                break;
+            }
+            case 0xC: {
+                break;
+            }
+            case 0xD: {
+                break;
+            }
+            case 0xE: {
+                break;
+            }
+            case 0xF: {
+                this.execOpr();
+                break;
+            }
+        }
+    }
+
+    execPfix() {
+        this.writeOreg(this.readOreg() << 4);
+        this.writeIptr(this.readIptr() + 1);
+    }
+
+    execNfix() {
+        this.writeOreg((~this.readOreg()) << 4);
+        this.writeIptr(this.readIptr() + 1);        
+    }
+
+    execOpr() {
+        switch (this.readOreg()) {
+
+        }
+        this.writeOreg(0);
+    }
+
+    execLdc() {
+        this.push(this.readOreg());
+        this.writeOreg(0);
+        this.writeIptr(this.nextInst());
+    }
+
+    nextInst(): number {
+        return this.registers[Regs.Iptr] + 1;
+    }
+
+    readIptr(): number {
+        return this.registers[Regs.Iptr];
+    }
+
+    writeIptr(value: number) {
+        this.registers[Regs.Iptr] = value;
+    }
+
+    readOreg(): number {
+        return this.registers[Regs.Oreg];
+    }
+
+    writeOreg(value: number) {
+        this.registers[Regs.Oreg] = value;
+    }
+
+    push(value: number) {
+        this.registers[Regs.Creg] = this.registers[Regs.Breg];
+        this.registers[Regs.Breg] = this.registers[Regs.Areg];
+        this.registers[Regs.Areg] = value;
+    }
+
+    top(): number {
+        return this.registers[Regs.Areg];
+    }
+
+    readByteMem(offset: number): number {
+        return this.memory[offset];
+    }
+
+    writeByteMem(offset: number, value: number) {
+        return this.memory[offset] = value;
+    }
+}
