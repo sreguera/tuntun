dc6a9ea54b5685ff8f4708b8cb2ff84819a7fedc
sreguera <sreguera@gmail.com>
2021-01-02 00:38:05 +0100

    Complete the assembler instruction list

--- src/Assembler.test.ts
+++ src/Assembler.test.ts
@@ -25,3 +25,17 @@ test('assembles a simple operation', () => {
     expect(bytes).toEqual([0xF0]);
 })
 
+test('assembles a complex operation', () => {
+    const bytes = asm('div');
+    expect(bytes).toEqual([0x22, 0xFC]);
+})
+
+test('assembles a super-complex operation', () => {
+    const bytes = asm('start');
+    expect(bytes).toEqual([0x21, 0x2F, 0xFF]);
+})
+
+test('assembles a fpentry operation', () => {
+    const bytes = asm('fpudivby2');
+    expect(bytes).toEqual([0x21, 0x41, 0x2A, 0xFB]);
+})
