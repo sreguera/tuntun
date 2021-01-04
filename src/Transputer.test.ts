9925ec20d3c1cb897df0b62096a5c3a5f648b811
sreguera <sreguera@gmail.com>
2021-01-04 11:45:37 +0100

    Refactor tests

--- src/Transputer.test.ts
+++ src/Transputer.test.ts
@@ -5,155 +5,138 @@ import { asm } from './Assembler';
  * Assembles the instructions in code and executes them. Stops after executing
  * as many steps as instructions. Do not use this for code with jumps!!!
  */
-function execSeq(code: string, t: Transputer) {
+function execSeq(code: string): Transputer {
+    const t = new Transputer();
     const codes = asm(code);
     codes.forEach((val, offset) => {
         t.writeByteMem(offset, val);
     });
     codes.forEach(_ => t.step());
+    return t;
 }
 
 /**
  * Assembles the instructions in code and executes them. Stops when a breakpoint
  * instruction is executed. Use this for code with jumps!!!
  */
-function execCFlow(code: string, t: Transputer) {
+function execCFlow(code: string): Transputer {
+    const t = new Transputer();
     const codes = asm(code);
     codes.forEach((val, offset) => {
         t.writeByteMem(offset, val);
     });
     t.run();
+    return t;
 }
 
 test('ldc loads a constant', () => {
-    const t = new Transputer();
-    execSeq('ldc 3', t);
+    const t = execSeq('ldc 3');
     expect(t.top()).toBe(0x3);
 });
 
 test('ldc loads a long constant', () => {
-    const t = new Transputer();
-    execSeq('ldc 0x345', t);
+    const t = execSeq('ldc 0x345');
     expect(t.top()).toBe(0x345);
 });
 
 test('ldc loads a negative constant', () => {
-    const t = new Transputer();
-    execSeq('ldc -2', t);
+    const t = execSeq('ldc -2');
     expect(t.top()).toBe(-2);
 });
 
 test('eqc produces true for equals', () => {
-    const t = new Transputer();
-    execSeq('ldc 3; eqc 3', t);
+    const t = execSeq('ldc 3; eqc 3');
     expect(t.top()).toBe(1);
 });
 
 test('eqc produces false for not equals', () => {
-    const t = new Transputer();
-    execSeq('ldc 3; eqc 5', t);
+    const t = execSeq('ldc 3; eqc 5');
     expect(t.top()).toBe(0);
 });
 
 test('rev reverses the stack', () => {
-    const t = new Transputer();
-    execSeq('ldc 3; ldc 4; rev', t);
+    const t = execSeq('ldc 3; ldc 4; rev');
     const a = t.pop();
     const b = t.pop();
     expect([a, b]).toEqual([3, 4]);
 });
 
 test('j jumps', () => {
-    const t = new Transputer();
-    execCFlow('j 2; ldc 3; j 0; ldc 5; j 0', t);
+    const t = execCFlow('j 2; ldc 3; j 0; ldc 5; j 0');
     expect(t.top()).toBe(5);
 });
 
 test('cj jumps if A is 0', () => {
-    const t = new Transputer();
-    execCFlow('ldc 0; cj 2; ldc 3; j 0; ldc 5; j 0', t);
+    const t = execCFlow('ldc 0; cj 2; ldc 3; j 0; ldc 5; j 0');
     expect(t.top()).toBe(5);
 });
 
 test(`cj doesn't jump if A is not 0`, () => {
-    const t = new Transputer();
-    execCFlow('ldc 1; cj 2; ldc 3; j 0; ldc 5; j 0', t);
+    const t = execCFlow('ldc 1; cj 2; ldc 3; j 0; ldc 5; j 0');
     expect(t.top()).toBe(3);
 });
 
 test('gcall jumps where A says', () => {
-    const t = new Transputer();
-    execCFlow('ldc 4; gcall; ldc 3; j 0; ldc 5; j 0', t);
+    const t = execCFlow('ldc 4; gcall; ldc 3; j 0; ldc 5; j 0');
     const a = t.pop();
     const b = t.pop();
     expect([a, b]).toEqual([5, 2]);
 });
 
 test('gajw changes the workspace pointer', () => {
-    const t = new Transputer();
-    execSeq('ldc 0x100; gajw; ldlp 0', t);
+    const t = execSeq('ldc 0x100; gajw; ldlp 0');
     expect(t.top()).toBe(0x100);
 });
 
 test('ajw adjusts the workspace pointer', () => {
-    const t = new Transputer();
-    execSeq('ldc 256; gajw; ajw -4; ldlp 0', t);
+    const t = execSeq('ldc 256; gajw; ajw -4; ldlp 0');
     expect(t.top()).toBe(240);
 });
 
 test('call calls a procedure', () => {
-    const t = new Transputer();
-    execCFlow('ldc 0x100; gajw; ldc 9; call 2; ldc 3; j 0; ldl 1; j 0', t);
+    const t = execCFlow('ldc 0x100; gajw; ldc 9; call 2; ldc 3; j 0; ldl 1; j 0');
     expect(t.top()).toBe(9);
 });
 
 test('ret returns from a procedure', () => {
-    const t = new Transputer();
-    execCFlow('ldc 256; gajw; call 2; ldlp 0; j 0; ldlp 0; ret', t);
+    const t = execCFlow('ldc 256; gajw; call 2; ldlp 0; j 0; ldlp 0; ret');
     const a = t.pop();
     const b = t.pop();
     expect([a, b]).toEqual([256, 240]);
 });
 
 test('ldl and stl load and set items in the workspace', () => {
-    const t = new Transputer();
-    execSeq('ldc 0x100; gajw; ldc 7; stl 0; ldc 2; ldl 0', t);
+    const t = execSeq('ldc 0x100; gajw; ldc 7; stl 0; ldc 2; ldl 0');
     expect(t.top()).toBe(7);
 });
 
 test('ldnlp computes a global address', () => {
-    const t = new Transputer();
-    execSeq('ldc 100; ldnlp 4', t);
+    const t = execSeq('ldc 100; ldnlp 4');
     expect(t.top()).toBe(116);
 });
 
 test('ldnl and stnl load and set items globally', () => {
-    const t = new Transputer();
-    execSeq('ldc 7; ldc 0x100; stnl 5; ldc 0; ldc 0x100; ldnl 5', t);
+    const t = execSeq('ldc 7; ldc 0x100; stnl 5; ldc 0; ldc 0x100; ldnl 5');
     expect(t.top()).toBe(7);
 });
 
 test('store and load of the status register works', () => {
-    const t = new Transputer();
-    execSeq('ldc 7; teststs; ldc 0; testlds', t);
+    const t = execSeq('ldc 7; teststs; ldc 0; testlds');
     expect(t.top()).toBe(7);
 });
 
 test('store and load of the D register works', () => {
-    const t = new Transputer();
-    execSeq('ldc 7; teststd; ldc 0; testldd', t);
+    const t = execSeq('ldc 7; teststd; ldc 0; testldd');
     expect(t.top()).toBe(7);
 });
 
 test('store and load of the E register works', () => {
-    const t = new Transputer();
-    execSeq('ldc 7; testste; ldc 0; testlde', t);
+    const t = execSeq('ldc 7; testste; ldc 0; testlde');
     expect(t.top()).toBe(7);
 });
 
 test('pop cycles the stack', () => {
-    const t = new Transputer();
-    execSeq('ldc 3; ldc 2; ldc 1; pop', t);
+    const t = execSeq('ldc 3; ldc 2; ldc 1; pop');
     const a = t.pop();
     const b = t.pop();
     const c = t.pop();
@@ -161,8 +144,7 @@ test('pop cycles the stack', () => {
 });
 
 test('dup duplicates the top of the stack', () => {
-    const t = new Transputer();
-    execSeq('ldc 1; ldc 2; dup', t);
+    const t = execSeq('ldc 1; ldc 2; dup');
     const a = t.pop();
     const b = t.pop();
     const c = t.pop();
@@ -170,131 +152,110 @@ test('dup duplicates the top of the stack', () => {
 });
 
 test('and performs a bitwise and', () => {
-    const t = new Transputer();
-    execSeq('ldc 0x0FF; ldc 0xFF0; and', t);
+    const t = execSeq('ldc 0x0FF; ldc 0xFF0; and');
     expect(t.top()).toBe(0x0F0);
 });
 
 test('or performs a bitwise or', () => {
-    const t = new Transputer();
-    execSeq('ldc 0x0FF; ldc 0xFF0; or', t);
+    const t = execSeq('ldc 0x0FF; ldc 0xFF0; or');
     expect(t.top()).toBe(0xFFF);
 });
 
 test('xor performs a bitwise xor', () => {
-    const t = new Transputer();
-    execSeq('ldc 0x0FF; ldc 0xFF0; xor', t);
+    const t = execSeq('ldc 0x0FF; ldc 0xFF0; xor');
     expect(t.top()).toBe(0xF0F);
 });
 
 test('not performs a bitwise not', () => {
-    const t = new Transputer();
-    execSeq('ldc 0xF; not', t);
+    const t = execSeq('ldc 0xF; not');
     expect(t.top() >>> 0).toBe(0xFFFFFFF0);
 });
 
 test('testerr with no error puts true on the stack', () => {
-    const t = new Transputer();
-    execSeq('testerr', t);
+    const t = execSeq('testerr');
     expect(t.top()).toBe(1);
 });
 
 test('testerr with error puts false on the stack', () => {
-    const t = new Transputer();
-    execSeq('seterr; testerr', t);
+    const t = execSeq('seterr; testerr');
     expect(t.top()).toBe(0);
 });
 
 test('testerr clear the error flag', () => {
-    const t = new Transputer();
-    execSeq('seterr; testerr; testerr', t);
+    const t = execSeq('seterr; testerr; testerr');
     expect(t.top()).toBe(1);
 });
 
 test('adc sums a constant to the top of the stack with no error', () => {
-    const t = new Transputer();
-    execSeq('ldc 4; adc 5; testerr', t);
+    const t = execSeq('ldc 4; adc 5; testerr');
     const a = t.pop();
     const b = t.pop();
     expect([a, b]).toEqual([1, 9]);
 });
 
 test('adc generates error on overflow', () => {
-    const t = new Transputer();
-    execSeq('ldc 0x7FFFFFFF; adc 1; testerr', t);
+    const t = execSeq('ldc 0x7FFFFFFF; adc 1; testerr');
     expect(t.top()).toBe(0);
 });
 
 test('adc generates error on underflow', () => {
-    const t = new Transputer();
-    execSeq('ldc 0x80000000; adc -1; testerr', t);
+    const t = execSeq('ldc 0x80000000; adc -1; testerr');
     expect(t.top()).toBe(0);
 });
 
 test('gt is true when b > a', () => {
-    const t = new Transputer();
-    execSeq('ldc 4; ldc 2; gt', t);
+    const t = execSeq('ldc 4; ldc 2; gt');
     expect(t.top()).toBe(1);
 });
 
 test('gt is false when b <= a', () => {
-    const t = new Transputer();
-    execSeq('ldc 4; ldc 4; gt', t);
+    const t = execSeq('ldc 4; ldc 4; gt');
     expect(t.top()).toBe(0);
 });
 
 test('shl shifts left', () => {
-    const t = new Transputer();
-    execSeq('ldc 0x80000000; ldc 4; shl', t);
+    const t = execSeq('ldc 0x80000000; ldc 4; shl');
     expect(t.top()).toBe(0x08000000);
 });
 
 test('shl shifts right', () => {
-    const t = new Transputer();
-    execSeq('ldc 0xFF; ldc 4; shr', t);
+    const t = execSeq('ldc 0xFF; ldc 4; shr');
     expect(t.top()).toBe(0xFF0);
 });
 
 test('mint produces the most negative int', () => {
-    const t = new Transputer();
-    execSeq('mint', t);
+    const t = execSeq('mint');
     expect(t.top()).toBe(-(2**31));
 });
 
 test('bcnt counts the number of bytes in words', () => {
-    const t = new Transputer();
-    execSeq('ldc 3; bcnt', t);
+    const t = execSeq('ldc 3; bcnt');
     expect(t.top()).toBe(12);
 });
 
 test('wcnt counts the number of words in bytes', () => {
-    const t = new Transputer();
-    execSeq('ldc 0xFF; wcnt', t);
+    const t = execSeq('ldc 0xFF; wcnt');
     const a = t.pop();
     const b = t.pop();
     expect([a, b]).toEqual([0x3F, 0x3]);
 });
 
 test('bsub produces the byte index', () => {
-    const t = new Transputer();
-    execSeq('ldc 3; ldc 5; bsub', t);
+    const t = execSeq('ldc 3; ldc 5; bsub');
     expect(t.top()).toBe(8);
 });
 
 test('wsub produces the word index', () => {
-    const t = new Transputer();
-    execSeq('ldc 3; ldc 5; wsub', t);
+    const t = execSeq('ldc 3; ldc 5; wsub');
     expect(t.top()).toBe(17);
 });
 
 test('wsubdb produces the double word index', () => {
-    const t = new Transputer();
-    execSeq('ldc 3; ldc 5; wsubdb', t);
+    const t = execSeq('ldc 3; ldc 5; wsubdb');
     expect(t.top()).toBe(29);
 });
 
 test('ldpi produces the byte index from the next inst', () => {
-    const t = new Transputer();
-    execSeq('ldc 5; ldpi', t);
+    const t = execSeq('ldc 5; ldpi');
     expect(t.top()).toBe(8);
 });
